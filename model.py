981cea209ec4ab59e2b30f7858c87fe2bfce9592
amiya <amiya19mandal@gmail.com>
2019-04-21 18:40:40 +0530

    added code for music gen

---
+++ model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import pdb
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size=10000, hidden_size=5000, output_size=10000, n_layers=1):
+        super(RNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.liner = nn.Linear(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size, n_layers)
+        self.decoder = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax()
+    
+    def forward(self, input_val, hidden):
+        input_val = input_val.view(-1, 10000)
+        input_val = self.liner(input_val)
+        input_val = input_val.view(1, 1, -1)
+        output, hidden = self.gru(input_val, hidden)
+        output = self.decoder(output.view(1, -1))
+        output = self.softmax(output)
+        return output, hidden
+
+    def init_hidden(self):
+        return Variable(torch.zeros(self.n_layers, 1, self.hidden_size))
---
+++ traing_data.py
@@ -0,0 +1,37 @@
+import glob
+import random
+import numpy as np 
+import torch
+from scipy.io.wavfile import read
+from sklearn import preprocessing
+import sys
+import pdb
+
+class GetSequence:
+
+    def __init__(self):
+        self.all_files = list(glob.iglob('data/*.wav'))
+        self.split_at = 10000
+        self.min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
+
+    def get_sequnce(self):
+        while True:
+            temp = 0
+            temp_file = random.choice(self.all_files)
+            value_array = read(temp_file)
+            value_array = value_array[1]
+            self.min_max_scaler.fit(value_array.astype(float))
+            value_array = self.min_max_scaler.transform(value_array)
+            # value_array = value_array/ np.linalg.norm(value_array)
+            value_array = np.delete(value_array, 1)
+            seq = value_array.shape
+            while temp < seq[0]:
+                in_val = torch.from_numpy(value_array[temp * self.split_at : (temp+1) * self.split_at])
+                # lab_val = torch.from_numpy(value_array[(temp * self.split_at) + self.diff : ((temp+1) * self.split_at) + self.diff])
+                lab_val = torch.tensor([value_array[((temp+1) * self.split_at) + 1]])
+                temp += self.split_at
+                yield in_val, lab_val
+                
+    def get_train_data(self):
+        in_lab , lab_val = next(self.get_sequnce())
+        return in_lab, lab_val
